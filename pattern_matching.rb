5e0fa24a5e63f342eff88be24cabc80e4bcec068
palashkolk <203395566+palashkolk@users.noreply.github.com>
2025-06-04 12:35:03 +0530

    More match types added as code snippets
    Following along the ruby pattern match chapter
    of the ODIN project.

--- pattern_matching.rb
+++ pattern_matching.rb
@@ -25,20 +25,41 @@ end
 ############### Variable pattern #################
 age = 15
 case age
-in a
+in a # Pattern match makes sure a = age
   puts a
 end
 
 a = 5
 case 1
-in a
+in a # no pinning operator makes sure a = 1 
   puts a
 end
 
 a = 7
 case 1
-in ^a
+in ^a  # pinning operator makes sure a=7; so 7 is match against 1
   puts 'match'
 else puts 'No match'
 end
-#############################
+############### As pattern match ##############
+case 10
+in 10 => a
+  puts a
+end
+
+########### Alternative pattern macth ##############
+a = 10
+case 10
+in 0|1|^a => var
+  puts "Match found: #{var}"
+else puts "No match found"
+end
+
+############## Guard conditions ###############
+some_other_value = false
+
+case 0
+in 0 if some_other_value
+  puts :match
+else puts :no_match
+end
