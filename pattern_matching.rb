134f4dcc314c7443236ec57ccae52356ae6794e6
palashkolk <203395566+palashkolk@users.noreply.github.com>
2025-06-04 12:10:04 +0530

    More code snippets from the ODIN project
    Follows along the ODIN project pattern matching
    exercise of ruby course.

--- pattern_matching.rb
+++ pattern_matching.rb
@@ -3,6 +3,7 @@ login = {username:'hornby', password: 'iliketrains'}
 login => {username:username}
 puts "Logged in with username #{username}"
 
+############### Return values #################
 grade ='C'
 
 result = case grade
@@ -13,3 +14,31 @@ result = case grade
 end
 
 puts result
+################ Object matching ################
+input = '3'
+
+case input
+in String then puts 'Input was of type String'
+in Integer then puts 'Input was of type Integer'
+end
+
+############### Variable pattern #################
+age = 15
+case age
+in a
+  puts a
+end
+
+a = 5
+case 1
+in a
+  puts a
+end
+
+a = 7
+case 1
+in ^a
+  puts 'match'
+else puts 'No match'
+end
+#############################
